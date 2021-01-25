8e83f6e01837a025a62cca2e66691fa86a62ed4d
Azamat ALIMBAYEV <azakost@gmail.com>
2021-01-25 13:49:25 +0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(CoreWidget());
 
-class MyApp extends StatelessWidget {
+class CoreWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]); // Portrait mode only
+    SystemChrome.setEnabledSystemUIOverlays([]); // Hide statusbar
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Text'),
